006a87ba182e72d036f5dcd2db0d8dd0f0d5b3fb
Juxhin (Eugene) Bakalli <sofokli1@gmail.com>
2019-01-22 12:36:41 +0100

    💄

--- Toggl.Foundation.MvvmCross/ViewModels/StartTimeEntryViewModel.cs
+++ Toggl.Foundation.MvvmCross/ViewModels/StartTimeEntryViewModel.cs
@@ -741,7 +741,8 @@ namespace Toggl.Foundation.MvvmCross.ViewModels
             var firstSuggestion = suggestions.FirstOrDefault();
             if (firstSuggestion is ProjectSuggestion)
                 return suggestions
-                    .OrderBy(ps => ((ProjectSuggestion)ps).ProjectName)
+                    .Cast<ProjectSuggestion>()
+                    .OrderBy(ps => ps.ProjectName)
                     .GroupByWorkspaceAddingNoProject()
                     .OrderByDefaultWorkspaceAndName(defaultWorkspace?.Id ?? 0);
 
